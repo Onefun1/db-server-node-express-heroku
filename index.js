aaf31610929c83e3616b568405d65834e6e7a202
MAKSYM_enko <finside41@gmail.com>
2019-07-31 15:05:31 +0300

    created server with test page

---
+++ index.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const fs = require("fs");
+const bodyParser = require("body-parser");
+const app = express();
+const port = process.env.PORT || 3000;
+
+let movies = [];
+readMovies();
+
+function readMovies() {
+  fs.readFile("./db.json", "utf8", function(err, data) {
+    if (err) {
+      return;
+    }
+
+    movies = JSON.parse(data);
+  });
+}
+
+function writeMovies() {
+  fs.writeFile("./db.json", JSON.stringify(movies), function(err) {
+    if (err) {
+      return;
+    }
+    console.log("Saved!");
+  });
+}
+
+app.use(bodyParser.text());
+
+app.get("/", (req, res) => res.send(`Hello from Github`));
+app.get("/movies", (req, res) => {
+  res.set("Access-Control-Allow-Origin", "*");
+  res.json(movies);
+});
+
+app.post("/movies", (req, res) => {
+  const newMovie = JSON.parse(req.body);
+  movies.push(newMovie);
+  writeMovies();
+
+  res.set("Access-Control-Allow-Origin", "*");
+  res.json(movies);
+});
+
+app.post("/movies", (req, res) => {
+  fs.readFile("./db.json", "utf8", function(err, data) {
+    if (err) {
+      return;
+    }
+
+    movies = JSON.parse(data);
+    const newMovie = JSON.parse(req.body);
+    movies.push(newMovie);
+
+    fs.writeFile("./db.json", JSON.stringify(movies), function(err) {
+      if (err) {
+        return;
+      }
+
+      res.set("Access-Control-Allow-Origin", "*");
+      res.json(movies);
+    });
+  });
+});
+
+app.listen(port, () =>
+  console.log(`Example app listening on http://127.0.0.1:${port}!`)
+);
